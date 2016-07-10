a9e4c0901d49d25436755265f647dbc7554703f0
Valentin Squirelo <valentin.squirelo@gmail.com>
2016-07-10 10:44:52 +0200

    Create esp8266_button_http.ino

---
+++ esp8266_button_http.ino
@@ -0,0 +1,67 @@
+/*
+ *  Press a button and send a post request
+ *
+ */
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+const int buttonPin = D2;
+int buttonState = 0;
+String url = "https://ewiqmhfpwe.execute-api.eu-central-1.amazonaws.com/prod/";
+String gateway_token ="";
+void Request();
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(buttonPin, INPUT);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+    WiFiMulti.addAP("bla", "blablabla");
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Wait for WiFi... ");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    delay(500);
+}
+
+
+void loop() {
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH) {
+      Serial.println("pressed");
+      Request();
+    } else {
+      Serial.println("waiting for a press");
+    }
+    delay(250);
+}
+
+void Request() {
+   HTTPClient http;
+   http.begin(url);
+   http.addHeader("Content-Type", "application/json");
+   http.addHeader("x-api-key", gateway_token);
+   String payload = String("{"clickType"; "SINGLE" }"); 
+   Serial.print("POST payload: "); Serial.println(payload);
+   int httpCode = http.POST(payload);
+   Serial.print("HTTP POST Response: "); Serial.println(httpCode); // HTTP code 200 means ok 
+   http.end();
+   delay (5000);
+}
+
